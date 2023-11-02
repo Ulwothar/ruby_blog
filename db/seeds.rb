af644eb07015610bf241b688f488bc3bf3ce0fc2
Mateusz Krupa <mateuszkrp@gmail.com>
2023-11-02 00:25:03 +0000

    Added users and basc authentication. Linked users to posts. Made changes in nav bar.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(email: "example@email.com", password: "password", password_confirmation: "password")
+
+10.times do |x|
+    Post.create(title: "Title #{x}", body: "Body #{x} Example body", user_id: User.first.id)
+end
